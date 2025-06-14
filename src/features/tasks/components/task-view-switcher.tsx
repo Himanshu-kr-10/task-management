9c4f2206d890664913849f9a0b8c879687ce2321
Himanshu-kr-10 <himanshukumar10202@gmail.com>
2025-06-14 13:57:09 +0530

    calendar view is done

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -23,6 +23,7 @@ import { DataKanban } from "./data-kanban";
 import { useCallback } from "react";
 import { TaskStatus } from "../types";
 import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
+import { DataCalendar } from "./data-calendar";
 
 export const TaskViewSwitcher = () => {
 
@@ -99,8 +100,8 @@ export const TaskViewSwitcher = () => {
             <TabsContent value="kanban" className="mt-0">
               <DataKanban data={tasks?.documents ?? []} onChange={onKanbanChange} />
             </TabsContent>
-            <TabsContent value="calendar" className="mt-0">
-              
+            <TabsContent value="calendar" className="mt-0 h-full">
+              <DataCalendar data={tasks?.documents ?? []} />
             </TabsContent>
           </>
         )}
