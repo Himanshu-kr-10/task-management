51d5e5cfb050176acec5b4204267925b0e4c259e
Himanshu-kr-10 <himanshukumar10202@gmail.com>
2025-06-14 08:31:22 +0530

    kanban-board done

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -20,6 +20,9 @@ import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { DataKanban } from "./data-kanban";
+import { useCallback } from "react";
+import { TaskStatus } from "../types";
+import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
 
 export const TaskViewSwitcher = () => {
 
@@ -47,6 +50,16 @@ export const TaskViewSwitcher = () => {
    });
 
   const { open } = useCreateTaskModal();
+
+  const { mutate: bulkUpdate } = useBulkUpdateTasks();
+
+  const onKanbanChange = useCallback((
+    tasks: { $id: string; status: TaskStatus; position: number }[]
+  ) => {
+    bulkUpdate({
+      json: { tasks }
+    })
+  }, [bulkUpdate])
 
   return (
     <Tabs 
@@ -84,10 +97,10 @@ export const TaskViewSwitcher = () => {
               <DataTable columns={columns} data={tasks?.documents ?? []}/>
             </TabsContent>
             <TabsContent value="kanban" className="mt-0">
-              <DataKanban data={tasks?.documents ?? []} />
+              <DataKanban data={tasks?.documents ?? []} onChange={onKanbanChange} />
             </TabsContent>
             <TabsContent value="calendar" className="mt-0">
-              Calendar view
+              
             </TabsContent>
           </>
         )}
